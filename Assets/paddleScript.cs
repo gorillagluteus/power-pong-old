3f300776f5d4e84df9823812e4a963393f7f7f14
grumblefart <bethebaconegg@gmail.com>
2019-07-03 13:42:23 -0400

    further work

---
+++ Assets/paddleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleScript : MonoBehaviour
+{
+	public GameObject hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	this.transform.position = hand.transform.position;
+    	this.transform.rotation = hand.transform.rotation * Quaternion.Euler(0,0,0);
+    }
+}
