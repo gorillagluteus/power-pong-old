efaa51fa04394d6d25876cebe437a1b0c48a6c5e
grumblefart <bethebaconegg@gmail.com>
2019-07-04 14:54:32 -0400

    more network testing

---
+++ Assets/PlayerObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		if(isLocalPlayer == false)    
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
