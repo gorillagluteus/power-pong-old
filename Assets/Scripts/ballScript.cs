8e9a0ca91a0b46222e4a3aba1ae62df81c13c091
grumblefart <52465526+grumblefart@users.noreply.github.com>
2019-07-16 14:42:35 -0400

    updates

--- Assets/Scripts/ballScript.cs
+++ Assets/Scripts/ballScript.cs
@@ -7,7 +7,7 @@ public class ballScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Rigidbody>().AddForce(-2, 1, 1, ForceMode.Impulse);
     }
 
     // Update is called once per frame
